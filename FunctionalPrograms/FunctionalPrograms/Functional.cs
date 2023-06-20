2825de0cbf741277f3c55911127907ab881c2a77
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-20 16:52:12 -0400

    [Ruchita] Add: Leap year program

---
+++ FunctionalPrograms/FunctionalPrograms/Functional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Functional
+    {
+        public void Check_year()
+        {
+            //Check year is leap or not.
+            Console.WriteLine("Enter the year wheather it is leap year or not");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("Year is a leap year");
+            }
+           else
+            {
+                Console.WriteLine("Year is not a leap year");
+            }
+        }
+    }
+}
---
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Program
+    {
+        public static void  Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Choice");
+            int choice=Convert.ToInt32(Console.ReadLine()); 
+            switch (choice) 
+            {
+                case 1:
+                    Console.WriteLine("The Leap year Program");
+                    Functional functional = new Functional();
+                    functional.Check_year();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
